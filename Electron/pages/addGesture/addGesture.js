9510a58bdff59fc4b009557f977682881d98875e
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-02-28 15:25:27 +0530

    gesture video capture

---
+++ Electron/pages/addGesture/addGesture.js
@@ -0,0 +1,54 @@
+function startRecording(thisButton){
+	navigator.mediaDevices.getUserMedia({
+			audio: false,
+			video: true,
+		}).then((mediaStream) => {
+			let chunks = [];
+			const mediaRecorder =
+			new MediaRecorder(mediaStream);
+
+			//Make the mediaStream global
+			window.mediaStream = mediaStream;
+			//Make the mediaRecorder global
+			window.mediaRecorder = mediaRecorder;
+
+			mediaRecorder.start();
+			const duration = 11000;
+			setTimeout(function(){
+				stopRecording();
+				thisButton.disabled = false;
+			},duration);
+			mediaRecorder.ondataavailable = (e) => {
+				chunks.push(e.data);
+			};
+			mediaRecorder.onstop = () => {
+				const blob = new Blob(
+					chunks, {
+						type: "video/mp4" 
+					});
+				chunks = [];
+				
+				const recordedMediaURL = URL.createObjectURL(blob);
+				window.Bridge.sendUrl(recordedMediaURL);
+			}
+			const webCamContainer = document.getElementById("web-cam-container");
+			webCamContainer.srcObject = mediaStream;
+			document.getElementById(
+				`vid-record-status`)
+				.innerText = "Recording";
+			thisButton.disabled = true;
+		})
+}
+
+function stopRecording() {
+	window.mediaRecorder.stop();
+	window.mediaStream.getTracks()
+	.forEach((track) => {
+		track.stop();
+		track.enabled = false;
+	});
+	document.getElementById(
+		`vid-record-status`)
+		.innerText = "Recording done!";
+	
+}
