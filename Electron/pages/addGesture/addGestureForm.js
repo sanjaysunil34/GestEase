2543e8b68d33eb7615f087cc2ee0d544191037e3
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-03-19 16:06:30 +0530

    button styles

--- Electron/pages/addGesture/addGestureForm.js
+++ Electron/pages/addGesture/addGestureForm.js
@@ -9,14 +9,14 @@ var element = document.getElementById('addGestureFormButton');
 element.setAttribute('href', document.referrer);
 element.onclick = function() {
     var functionaility = document.getElementById('func').value;
-    var binding = document.getElementById('bind').value;
-    console.log(functionaility);
-    console.log(binding);
-    var res = functionaility.concat('\n');
-    res = res.concat(binding);
-    const blob = new Blob([res], {type: "text/plain"});
-    const fileUrl = URL.createObjectURL(blob);
-    window.Bridge.sendTXTUrl(fileUrl);
+    // var binding = document.getElementById('bind').value;
+    // console.log(functionaility);
+    // console.log(binding);
+    // var res = functionaility.concat('\n');
+    // res = res.concat(binding);
+    // const blob = new Blob([res], {type: "text/plain"});
+    // const fileUrl = URL.createObjectURL(blob);
+    // window.Bridge.sendTXTUrl(fileUrl);
     location.replace("./addGesture.html")
   return false;
 }
