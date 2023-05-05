7516cb8255cbf12bdea9d1614b0c00d70e902a83
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-05-05 10:33:57 +0530

    automatic updation of manual page

--- Electron/main.js
+++ Electron/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow,ipcMain } = require("electron");
 const path = require("path");
+const fs = require("fs");
 const {download} = require('electron-dl');
 const { spawn } = require('child_process');
 require("electron-reload")(__dirname, {
@@ -125,4 +126,30 @@ ipcMain.on("voice",async (event, command) => {
         console.log('STOPPING GESTEASE - voice');
         child.kill('SIGTERM');
     }
+});
+
+function openFile() {
+    return new Promise((resolve, reject) => {
+        fs.readFile("../python_scripts/gesture/db.json", "utf-8", (error, data) => {
+            if (error) {
+                console.log('reject: ' + error); // Testing
+                reject(error);
+            } else {
+                console.log('resolve: ' + data); // Testing
+                resolve(data)
+            }
+        });
+    });
+}
+
+ipcMain.handle('load-file', async (event, message) => {
+    return await openFile()
+        .then((data) => {
+            console.log('handle: ' + data); // Testing
+            return data;
+        })
+        .catch((error) => {
+            console.log('handle error: ' + error); // Testing
+            return 'Error Loading Log File';
+        })
 });
--- python_scripts/gesture/database.py
+++ python_scripts/gesture/database.py
@@ -1,5 +1,8 @@
 import os
 from tinydb import TinyDB, Query
+dirname = os.path.dirname(__file__)
+parent = os.path.dirname(dirname)
+main_dir = os.path.dirname(parent)
 db_path = os.path.join(os.getcwd())
 path_str=db_path.split('Electron')
 new=path_str[0]+'python_scripts/gesture/db.json'
@@ -20,6 +23,16 @@ def list_actions():
 def write_keys(action,keys):
     image = action + '.jpg'
     db.insert({'action': action, 'keys': keys, 'image': image})
+    write_to_manual()
+
+def write_to_manual():
+    gesture_file = os.path.join(main_dir, 'Electron/gestureFile/manual.txt')
+    f=open(gesture_file,"w")
+    for item in db:
+        str=''
+        str=item['action']+' '+item['keys']+' '+item['image']+'\n'
+        print(str)
+        f.writelines(str)
 
 def search_keys(action):
     temp=db.search(act.action==action)
